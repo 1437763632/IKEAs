c980a760c1f7fb0d45bdd9271193eb92c60a72da
1437763632 <1437763632@qq.com>
2018-12-07 18:47:37 +0800

    Pam

--- IKEA/IKEA.IServices/IRightServices.cs
+++ IKEA/IKEA.IServices/IRightServices.cs
@@ -8,10 +8,27 @@ namespace IKEA.IServices
 {
     using IKEA.Model;
     /// <summary>
-    /// 
+    /// 权限管理接口
     /// </summary>
     public interface IRightServices
     {
+        /// <summary>
+        /// 添加权限
+        /// </summary>
+        /// <param name="right"></param>
+        /// <returns></returns>
         int Add(TRight right);
+        /// <summary>
+        /// 修改权限信息
+        /// </summary>
+        /// <param name="right"></param>
+        /// <returns></returns>
+        int Update(TRight right);
+        /// <summary>
+        /// 根据ID获取权限信息
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        TRight GetRight(int id);
     }
 }
